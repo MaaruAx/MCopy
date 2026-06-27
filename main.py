d82bf71258ac06633357019351ba2331ebd62c5e
MaaruAx <maaruax@gmail.com>
2026-06-26 22:02:04 -0600

    refactor(ui): remove redundant high DPI attribute
    - Remove Qt.AA_UseHighDpiPixmaps attribute setting from QApplication initialization as it is deprecated in modern Qt versions and enabled by default.

--- main.py
+++ main.py
@@ -1055,7 +1055,6 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setApplicationName("MCopy")
     app.setOrganizationName("MMarket")
-    app.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
     app.setAttribute(Qt.AA_ShareOpenGLContexts, True)
 
     window = MCopyWindow()
